7b4d47c6a844cb3ed59ecfb6e77516987fab495e
TerevaMillet <59101444+TerevaM@users.noreply.github.com>
2022-11-03 13:53:12 +0100

    Update comp_base.php

--- compenents/comp_base/comp_base.php
+++ compenents/comp_base/comp_base.php
@@ -12,7 +12,7 @@ function start_page($title, $link_css) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="<?php if($link_css): print_r($link_css); endif;?>style.css">
+    <link rel="stylesheet" href="<?php if($link_css): return $link_css; endif;?>style.css">
     <title><?= $title ?></title>
 </head>
 <body class="bg-dark text-white">
